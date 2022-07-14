a36394747336eae332d34ebdae7ac6ecc3ce6635
BirmiiiYo <avramenkoa773@gmail.com>
2022-07-14 22:38:00 +0300

    adaptive to ~700px

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e25078ec4b203b3"; }
+/******/ 		__webpack_require__.h = function() { return "0eeed89eb54fb577"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
